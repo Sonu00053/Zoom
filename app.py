220e22a39cbbf88ad561675b6a25a4a8b63ada2c
Sonu00053 <sk6803028@gmail.com>
2026-05-19 16:48:06 +0530

    Fix zoom endpoint

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
 from fastapi import FastAPI
-from threading import Thread
-from controllers.User.Zoom import ZoomController
 
-app = FastAPI(
-    title="Zoom Automation API",
-    version="1.0.0"
-)
+app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Zoom Automation Server is Running!"}
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+    return {"status": "success", "message": "Railway app is running"}
 
 @app.get("/zoom")
 def zoom():
-    Thread(target=ZoomController.start, daemon=True).start()
-    return {
-        "status": "started",
-        "message": "Zoom bot started in background"
-    }
+    return {"message": "Zoom endpoint working"}
