528286b1e70cf7d598f096468edd1bce254339bb
Sonu00053 <sk6803028@gmail.com>
2026-05-20 11:34:14 +0530

    fix railway port issue final

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"status": "server working"}
+    return {"status": "alive"}
 
 @app.get("/health")
 def health():
     return {"ok": True}
+
+
+# 🔥 IMPORTANT: Railway safe start
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "app:app",
+        host="0.0.0.0",
+        port=port
+    )
